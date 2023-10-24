9c8925d81c8dbf935c1457777c853c39782728fa
Joy Fan <chandler0204@gmail.com>
2023-10-24 15:31:04 +0800

    Setting: install Material Design Icon and config Vuetify Theme

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,36 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const myCustomLightTheme = {
+    dark: false,
+    colors: {
+        background: '#FFFFFF',
+        surface: '#FFFFFF',
+        primary: '#FB8C00',
+        'primary-darken-1': '#3700B3',
+        secondary: '#03DAC6',
+        'secondary-darken-1': '#018786',
+        error: '#B00020',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+    },
+}
 
 const vuetify = createVuetify({
     components,
     directives,
+    theme: {
+        defaultTheme: 'myCustomLightTheme',
+        themes: {
+            myCustomLightTheme,
+        },
+    },
+    icons: {
+        defaultSet: 'mdi',
+    },
 })
 
 createApp(App).use(vuetify).mount('#app')
